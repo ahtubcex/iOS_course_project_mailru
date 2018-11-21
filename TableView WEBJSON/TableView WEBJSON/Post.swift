6ccbb91a2dd17faa9ff52df4b15d05590c9eca96
Артем Закиров <>
2018-11-21 17:44:48 +0300

    updated model4json

--- TableView WEBJSON/TableView WEBJSON/Post.swift
+++ TableView WEBJSON/TableView WEBJSON/Post.swift
@@ -12,9 +12,9 @@ import Foundation
 struct Post {
     
     let name : String
-    let genre : String
-    let episodes : String
-    let episodes_length : String
+    let type : String
+    let caliber : String
+    let rate_of_fire : String
     let description : String
 //    let image : UIImage
     
@@ -26,16 +26,16 @@ extension Post {
     init?(dict: NSDictionary) {
         guard
             let name = dict["name"] as? String,
-            let genre = dict["type"] as? String,
-            let episodes = dict["episodes"] as? String,
-            let episodes_length = dict["episode_length"] as? String,
+            let type = dict["type"] as? String,
+            let caliber = dict["caliber"] as? String,
+            let rate_of_fire = dict["rate_of_fire"] as? String,
             let description = dict["description"] as? String
             else { return nil }
         
         self.name = name
-        self.genre = genre
-        self.episodes = episodes
-        self.episodes_length = episodes_length
+        self.type = type
+        self.caliber = caliber
+        self.rate_of_fire = rate_of_fire
         self.description = description
     }
     
