6fadf544aad55b4ccc631da31e57904eb0ad440e
Ibragim <ahtubcex@ya.ru>
2018-11-21 16:50:39 +0300

    first commit, app with table

---
+++ hw-task2/dz2/Cell.swift
@@ -0,0 +1,26 @@
+//
+//  Cell_Img.swift
+//  dz2
+//
+//  Created by Ибрагим Мамадаев on 07.10.2018.
+//  Copyright © 2018 Ибрагим Мамадаев. All rights reserved.
+//
+
+import UIKit
+
+class Cell_Img: UITableViewCell {
+
+    @IBOutlet weak var full_img: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ hw-task2/dz2/MyCell.swift
@@ -0,0 +1,18 @@
+//
+//  Cell_Img.swift
+//  dz2
+//
+//  Created by Ибрагим Мамадаев on 07.10.2018.
+//  Copyright © 2018 Ибрагим Мамадаев. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    
+}
