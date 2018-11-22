d9c90a9fbeec1be6fffa4ab9a90344e48100fe58
Артем Закиров <>
2018-11-22 12:28:33 +0300

    new clean proj

---
+++ omnibox/omniBox/omniBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omniBox
+//
+//  Created by Артем Закиров on 22.11.2018.
+//  Copyright © 2018 bmstu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
