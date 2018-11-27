e5ac5577670a5a79a22daffb73c9771a849bb0d7
Артем Закиров <>
2018-11-27 15:42:01 +0300

    now adding order to realm without items

--- omnibox/omniBox/omniBox/AddOrder.swift
+++ omnibox/omniBox/omniBox/AddOrder.swift
@@ -22,11 +22,37 @@ class AddOrder: UIViewController {
     
     
     @IBAction func addButtonPressed(_ sender: Any) {
-        print(orderText.text)
-        print(fioText.text)
-        print(phoneText.text)
-        print(dataPick.date)
+        addOrder()
         self.dismiss(animated: true, completion: nil)
     }
+
+    func addOrder(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        let today = dateFormatter.string(from: dataPick.date)
+        let realm = try! Realm()
+        let new_order = Order()
+        new_order.number = self.orderText.text!
+        new_order.fio = self.fioText.text!
+        new_order.phone_number = self.phoneText.text!
+        new_order.arr_date = today
+        new_order.date_to = splitDate(st: today)
+        print(new_order)
+        
+        try! realm.write {
+            realm.add(new_order)
+        }
+    }
+    
+    func splitDate(st : String) -> String{     //adding 5 days
+        var date_to = st.components(separatedBy: "-")[0]
+        date_to = String(Int(date_to)!+5)+"-"+st.components(separatedBy: "-")[1]+"-"+st.components(separatedBy: "-")[2]
+        return date_to
+    }
+    
+    
     
 }
+
+
+
