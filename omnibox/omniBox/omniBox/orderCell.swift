7518549dfc6bddfab84dca52c5ca5eb4e93a568c
Артем Закиров <>
2018-11-28 00:23:14 +0300

    add orderCell

---
+++ omnibox/omniBox/omniBox/orderCell.swift
@@ -0,0 +1,20 @@
+//
+//  orderCell.swift
+//  omniBox
+//
+//  Created by Артем Закиров on 27.11.2018.
+//  Copyright © 2018 bmstu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RealmSwift
+
+class OrderCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var date_toLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var orderLabel: UILabel!
+    @IBOutlet weak var fioLabel: UILabel!
+}
